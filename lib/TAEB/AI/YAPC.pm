445273fcda6d3464874323c4c15d84a23669fae1
Jesse Luehrs <doy@tozt.net>
2009-06-23 16:20:35 -0500

    exploring

--- lib/TAEB/AI/YAPC.pm
+++ lib/TAEB/AI/YAPC.pm
@@ -4,6 +4,12 @@ extends 'TAEB::AI';
 
 sub next_action {
     my $self = shift;
+    my $path;
+    $self->currently('exploring');
+    $path = TAEB::World::Path->first_match(
+        sub { shift->unexplored }
+    );
+    return $path if $path;
     $self->currently('random walking');
     my $direction = (qw(y u h j k l b n))[int rand 8];
     return TAEB::Action::Move->new(direction => $direction);
