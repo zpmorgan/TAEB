3796f276983590280906c90f4d30fab3605f4bd8
Shawn M Moore <sartak@gmail.com>
2009-06-26 19:50:09 -0400

    Add a method is_inherently_unwalkable for stuff that is "usually" unwalkable
        Monsters and boulders are not inherently_unwalkable, but walls are

--- lib/TAEB/World/Tile.pm
+++ lib/TAEB/World/Tile.pm
@@ -291,6 +291,30 @@ sub is_walkable {
     return $is_walkable{ $self->type };
 }
 
+sub is_inherently_unwalkable {
+    my $self = shift;
+    my $through_unknown = shift;
+    my $dont_check_current_tile = shift;
+
+    # current tile is always walkable, but don't check it if our caller
+    # asked us not to (that check is rather slow)
+    return 1 if !defined($dont_check_current_tile)
+             && $self == TAEB->current_tile;
+
+    # traps are unpathable in Sokoban
+    return 0 if $self->type eq 'trap'
+             && $self->level->known_branch
+             && $self->level->branch eq 'sokoban';
+
+    # we can path through unlit areas that we haven't seen as rock for sure yet
+    # if we're blind, then all bets are off
+    return 1 if $through_unknown
+             && !TAEB->is_blind
+             && $self->type eq 'unexplored';
+
+    return $is_walkable{ $self->type };
+}
+
 sub update_lit {
     my $self = shift;
 
