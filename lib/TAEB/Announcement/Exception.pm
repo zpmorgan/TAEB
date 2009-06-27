79ad5c7e01a5d9b8e05748e452db6e0ffcbd5133
Shawn M Moore <sartak@gmail.com>
2009-06-26 22:16:13 -0400

    Add an announcement class for exceptions

---
+++ lib/TAEB/Announcement/Exception.pm
@@ -0,0 +1,10 @@
+package TAEB::Announcement::Exception;
+use TAEB::OO;
+extends 'TAEB::Announcement';
+
+sub immediate { 1 }
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
---
+++ lib/TAEB/Announcement/Exception/MissingItem.pm
@@ -0,0 +1,8 @@
+package TAEB::Announcement::Exception::MissingItem;
+use TAEB::OO;
+extends 'TAEB::Announcement::Exception';
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
