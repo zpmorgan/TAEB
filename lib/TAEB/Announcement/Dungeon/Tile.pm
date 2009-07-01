0a76bafcd07a65def87418d3231002db1257d3d5
Alex Smith <ais523@dell.linuxdev.us.dell.com>
2009-07-01 22:20:58 +0100

    We might not know TAEB->current_tile when constructing an announcement
    This was causing things like NoItems announcements to clear items on
    the tile we were stepping from, rather than the tile we're currently
    standing on.

--- lib/TAEB/Announcement/Dungeon/Tile.pm
+++ lib/TAEB/Announcement/Dungeon/Tile.pm
@@ -2,11 +2,28 @@ package TAEB::Announcement::Dungeon::Tile;
 use TAEB::OO;
 extends 'TAEB::Announcement';
 
+# We can't use TAEB->current_tile directly here, because when the
+# screenscraper sends an announcement meaning the 'current tile',
+# we don't know what the current tile is yet. We could update this
+# lazily, but that's semantically incorrect; instead, the value undef
+# for current_tile here means 'the current tile, once we know what it
+# is', and the reader allows for this. This implementation assumes that
+# we won't move between sending an announcement and receiving it, but
+# avoiding that would mean that announcements would need to respond to
+# other announcements.
 has tile => (
     is      => 'ro',
-    isa     => 'TAEB::World::Tile',
-    default => sub { TAEB->current_tile },
+    isa     => 'Maybe[TAEB::World::Tile]',
+    default => undef,
 );
+around tile => sub {
+    my $orig = shift;
+    my $self = shift;
+    # no need to worry about arguments, this is an ro attribute
+    my $tile = $orig->();
+    return $tile if $tile;
+    return TAEB->current_tile;
+};
 
 __PACKAGE__->meta->make_immutable;
 
