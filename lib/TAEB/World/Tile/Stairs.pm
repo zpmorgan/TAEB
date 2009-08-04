58fdd12a1dbb9424106c423ec7c4660ae83134e2
Alex Smith <ais523@dell.linuxdev.us.dell.com>
2009-08-04 03:14:44 +0100

    Keep stair other_sides consistent with each other
    If a tile we thought was stairs turns out not to be stairs after all,
    we have to mark the other_side of the hypothetical stairway as being
    unknown, as the place we thought it was going is obviously incorrect.

--- lib/TAEB/World/Tile/Stairs.pm
+++ lib/TAEB/World/Tile/Stairs.pm
@@ -23,6 +23,23 @@ override debug_color => sub {
          : super;
 };
 
+override change_type => sub {
+    my $self = shift;
+    my $newtype = shift;
+
+    # If we're replacing stairs by anything but stairs, obviously
+    # we're confused as to where the other stairs lead, and we should
+    # clear that value. This happens when, for instance, we end up
+    # somewhere other than the stairs when going downstairs (say if a
+    # monster followed us, and it was on the staircase, not us).
+    if ($newtype ne 'stairsup' && $newtype ne 'stairsdown') {
+        $self->other_side->clear_other_side if $self->other_side;
+    }
+
+    # Once we've done that, call the original procedure.
+    super;
+};
+
 __PACKAGE__->meta->make_immutable;
 
 1;
